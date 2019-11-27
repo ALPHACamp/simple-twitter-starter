b92d6c8efbf155d2ba13ee0cbc55223d4baf4e4e
KerwinJhong <sh940718@gmail.com>
2019-11-28 01:36:04 +0800

    create seed(user & tweet)

---
+++ seeders/20191127102558-seed-file.js
@@ -0,0 +1,51 @@
+'use strict';
+const bcrypt = require('bcrypt-nodejs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      role: "Admin",
+      name: 'root',
+      avatar: "",
+      introduction: "hi,i'm a User",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      role: "User",
+      name: 'user1',
+      avatar: "",
+      introduction: "hi,i'm a User",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      role: "User",
+      name: 'user2',
+      avatar: "",
+      introduction: "hi,i'm a User",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+
+    return queryInterface.bulkInsert('Tweets',
+      Array.from({ length: 20 }).map(d =>
+        ({
+          UserId: Math.floor(Math.random() * 3) + 1,
+          description: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Tweets', null, {})
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
