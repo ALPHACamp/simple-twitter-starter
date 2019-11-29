f9a48290f0e80f771fd90881b128238974f9eab6
Ignatius Fan <fan01856472@gmail.com>
2019-11-29 11:37:22 +0800

    pull new branch from origin/master

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 const tweetController = require('../controllers/tweetController.js')
-
+const userController = require('../controllers/userController.js')
 const adminController = require('../controllers/admin/adminController.js')
 
-module.exports = app => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => { res.redirect('/tweets') })
 
   app.get('/tweets', tweetController.getTweets)
 
+  // Sign up.in.out routes 
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
+
+
   // admin routes
   app.get('/admin/tweets', adminController.getTweets)
   app.get('/admin/users', adminController.getUsers)
+
 }
