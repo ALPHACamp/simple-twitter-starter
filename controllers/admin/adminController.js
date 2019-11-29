787689e9d064ef617dfc46408c656e0c7af5e73f
Ignatius Fan <fan01856472@gmail.com>
2019-11-29 12:22:24 +0800

    admin can manage all tweets and users

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,10 +1,25 @@
+const db = require('../../models')
+const Tweet = db.Tweet
+const User = db.User
+
+
 const adminController = {
   getTweets: (req, res) => {
-    return res.render('admin/tweets')
+    return Tweet.findAll({ include: [User] }).then(tweets => {
+      tweets = tweets.map(tweet => ({
+        ...tweet.dataValues,
+        description: tweet.dataValues.description.substring(0, 50),
+        username: tweet.dataValues.User.name
+      }))
+      console.log('tweets:', tweets)
+      return res.render('admin/tweets', { tweets: tweets })
+    })
   },
 
   getUsers: (req, res) => {
-    return res.render('admin/users')
+    return User.findAll().then(users => {
+      res.render('admin/users', { users: users })
+    })
   }
 }
 
