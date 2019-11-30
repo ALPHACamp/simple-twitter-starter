ddcb57153b4e3088f80d02555c3534d3d17b5a9d
Ignatius Fan <fan01856472@gmail.com>
2019-11-30 15:51:29 +0800

    user can see all tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,6 +1,24 @@
+const db = require('../models')
+const strftime = require('strftime')
+const Tweet = db.Tweet
+const User = db.User
+
 const tweetController = {
   getTweets: (req, res) => {
-    return res.render('tweets')
+    return Tweet.findAll({ include: [User], order: [['updatedAt', 'DESC']] }).then(tweets => {
+      tweets = tweets.map(tweet => ({
+        ...tweet.dataValues,
+        description: tweet.dataValues.description.substring(0, 140),
+        createdAt: strftime('%Y-%m-%d, %H:%M', tweet.dataValues.createdAt)
+      }))
+      User.findAll({ limit: 10 }).then(users => {
+        users = users.map(user => ({
+          ...user.dataValues,
+          introduction: user.dataValues.introduction.substring(0, 100)
+        }))
+        return res.render('tweets', { users: users, tweets: tweets })
+      })
+    })
   }
 
 }
