7b71499fcaf015b95282103d6c9606f9f96f12b3
Luwengier <g217881111@gmail.com>
2019-12-06 17:34:26 +0800

    Fix check of username in signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,16 +26,23 @@ const userController = {
       // confirm unique user
       User.findOne({ where: { email: req.body.email } }).then(user => {
         if (user) {
-          req.flash('error_messages', '信箱重複！')
+          req.flash('error_messages', '用戶信箱重複！')
           return res.redirect('/signup')
         } else {
-          User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-          }).then(user => {
-            req.flash('success_messages', '成功註冊帳號！')
-            return res.redirect('/signin')
+          User.findOne({ where: { name: req.body.name } }).then(user => {
+            if (user) {
+              req.flash('error_messages', '用戶名稱重複！')
+              return res.redirect('/signup')
+            } else {
+              User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+              }).then(user => {
+                req.flash('success_messages', '成功註冊帳號！')
+                return res.redirect('/signin')
+              })
+            }
           })
         }
       })
